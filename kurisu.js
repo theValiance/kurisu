52657ba7c15dab1ec5d6e38eaf20b493791260ce
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-04-21 18:38:07 -0400

    Update kurisu.js

--- kurisu.js
+++ kurisu.js
@@ -1,8 +1,8 @@
 //set up the discord client
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const exec = require('child_process').exec;
-const token = 'Mjg1NTkxNjU4MTY1NDM2NDE2.C9wPGQ.s7zsiACduR2oxmAzw9WMCw3t-ok';
+const var Discord = require("discord.js");
+const var client = new Discord.Client();
+const var spawn = require('child_process').spawn;
+const var token = 'Mjg1NTkxNjU4MTY1NDM2NDE2.C9wPGQ.s7zsiACduR2oxmAzw9WMCw3t-ok';
 
 client.on('ready', () => {
 	console.log('Logged in as ${client.user.username}');
@@ -12,17 +12,17 @@ client.on('message', msg => {
 	if (msg.content === 'update'){
 		msg.reply('Checking for updates...')
 			.then(() =>{
-				exec('git pull origin', (error, stdout, stderr) =>{
-					console.log('stdout: ${stdout}');
-					console.log('stderr: ${stderr}');
-					if (error !== null){
-						console.log('exec error: ${error}');	
-					}
+				var gitProc = spawn('git pull origin');
+			
+				//when there is some data output
+				gitProc.stdout.on('data', (chunk) =>{
+					console.log(chunk);
 				});
-			})
-			.then(() =>{
-				process.exit(0);
-		});
+			
+				gitProc.on('close', (code) => {
+					process.exit(0);	
+				});
+			});
 	}
 });
 
