1747602fd6d8130b15b605dd29f778b7bfcad461
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-04-21 18:27:37 -0400

    git shell command output is logged

--- kurisu.js
+++ kurisu.js
@@ -5,17 +5,23 @@ const exec = require('child_process').exec;
 const token = 'Mjg1NTkxNjU4MTY1NDM2NDE2.C9wPGQ.s7zsiACduR2oxmAzw9WMCw3t-ok';
 
 client.on('ready', () => {
-	console.log('Logged in as ${client.user.username}!');
+	console.log('Logged in as ${client.user.username}');
 });
 
 client.on('message', msg => {
 	if (msg.content === 'update'){
 		msg.reply('Checking for updates...')
 			.then(() =>{
-				exec('git pull origin', function(error, stdout, stderr){});
+				exec('git pull origin', (error, stdout, stderr) =>{
+					console.log('stdout: ${stdout}');
+					console.log('stderr: ${stderr}');
+					if (error !== null){
+						console.log('exec error: ${error}');	
+					}
+				});
 			})
 			.then(() =>{
-				process.exit();
+				process.exit(0);
 		});
 	}
 });
