e1106eb994620b9241b74c0a4a9d356133ee6893
theValiance <spiritofvale@gmail.com>
2017-04-21 21:47:04 +0000

    Added basic discord connectivity/interactivity. Should be able to attempt an update via a discord message now...

--- kurisu.js
+++ kurisu.js
@@ -1,6 +1,19 @@
-var exec = require('child_process').exec;
-var cmd = 'git pull origin';
+//set up the discord client
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const exec = require('child_process').exec;
+const token = Mjg1NTkxNjU4MTY1NDM2NDE2.C9wPGQ.s7zsiACduR2oxmAzw9WMCw3t-ok;
 
-exec(cmd, function(error, stdout, stderr) {
-  // command output is in stdout
+client.on('ready', () => {
+	console.log('Logged in as ${client.user.username}!');
 });
+
+client.on('message', msg => {
+	if (msg.content === 'update'){
+		msg.reply('Checking for updates...');
+		exec('git pull origin', function(error, stdout, stderr){});
+		process.exit();
+	}
+}
+
+client.login(token);
