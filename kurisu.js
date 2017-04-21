e3122bbec09ae82e85a67eb447319f661e77412e
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-04-21 19:15:52 -0400

    Update kurisu.js

--- kurisu.js
+++ kurisu.js
@@ -1,23 +1,31 @@
 //set up the discord client
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const spawn = require('child_process').spawnSync;
+const exec = require('child_process').exec;
 const token = 'Mjg1NTkxNjU4MTY1NDM2NDE2.C9wPGQ.s7zsiACduR2oxmAzw9WMCw3t-ok';
 
 client.on('ready', () => {
-	console.log('Logged in as ${client.user.username}');
+	console.log(`Logged in as ${client.user.username}`);
 });
 
 client.on('message', msg => {
 	if (msg.content === 'update'){
 		var sentMessage = msg.reply('Checking for updates...')
 			.then((msg) =>{
-				var gitProc = spawn('git pull origin');
-			
-				console.log(gitProc.stdout)
-					.then(() =>{
+				var gitProc = exec('git pull origin', (error, stdout, stderr) => {
+					if (error) {
+    						console.error(`exec error: ${error}`);
+    						return;
+  					}
+  					console.log(`stdout: ${stdout}`);
+  					console.log(`stderr: ${stderr}`);
+					if (stdout === 'Already up-to-date.'){
+					
+					}
+					else{
 						process.exit(0);
-					});
+					}
+				});
 			});
 	}
 });
