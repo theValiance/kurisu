fe0943f5e30b8b7702cb35fb0a5252dba579a1e4
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-04-21 21:44:10 -0400

    Update kurisu.js

--- kurisu.js
+++ kurisu.js
@@ -16,38 +16,40 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-	if (msg.content === 'update'){
-		msg.channel.sendMessage('Checking for updates...')
-			.then((msg) =>{
-				var gitProc = exec('git pull origin', (error, stdout, stderr) => {
-					if (error) {
-    						console.error(`exec error: ${error}`);
-    						return;
-  					}
-  					console.log(`stdout: ${stdout}`);
-  					console.log(`stderr: ${stderr}`);
-					if (stdout === 'Already up-to-date.\n'){
-						msg.edit('Already up to date.');
-					}
-					else{
-						fs.readFile('CHANGELIST', 'utf8', (err, data) =>{
-							if (!err){
-								var startPos = data.indexOf('{');
-								var endPos = data.indexOf('}');
-								version = data.slice(0, startPos);
-								msg.edit(`Updated to version **${version}**`)
-									.then((msg) =>{
-										var changelist = data.slice(startPos+1, endPos-1);
-										msg.channel.sendMessage("```" + `${changelist}` + "```")
-											.then(()=>{
-												process.exit(0);
-											});
-									});
-							}
-						});	
-					}
+	if (msg.mentions.users.has(client.user.id)){
+		if (msg.content === 'update'){
+			msg.channel.sendMessage('Checking for updates...')
+				.then((msg) =>{
+					var gitProc = exec('git pull origin', (error, stdout, stderr) => {
+						if (error) {
+							console.error(`exec error: ${error}`);
+							return;
+						}
+						console.log(`stdout: ${stdout}`);
+						console.log(`stderr: ${stderr}`);
+						if (stdout === 'Already up-to-date.\n'){
+							msg.edit('Already up to date.');
+						}
+						else{
+							fs.readFile('CHANGELIST', 'utf8', (err, data) =>{
+								if (!err){
+									var startPos = data.indexOf('{');
+									var endPos = data.indexOf('}');
+									version = data.slice(0, startPos);
+									msg.edit(`Updated to version **${version}**`)
+										.then((msg) =>{
+											var changelist = data.slice(startPos+1, endPos-1);
+											msg.channel.sendMessage("```" + `${changelist}` + "```")
+												.then(()=>{
+													process.exit(0);
+												});
+										});
+								}
+							});	
+						}
+					});
 				});
-			});
+		}
 	}
 });
 
