4fb33d8e2c137e58c8f55e9e3f710c8811e1b98e
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-04-21 22:34:42 -0400

    Update kurisu.js

--- kurisu.js
+++ kurisu.js
@@ -31,9 +31,9 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+	var command = pullCommand(msg.content);
+	console.log(`${command}`);
 	if (msg.isMentioned(client.user)){
-		var command = pullCommand(msg.content);
-		console.log(`${command}`);
 		if (command == '!update'){
 			msg.channel.sendMessage('Checking for updates...')
 				.then((msg) =>{
