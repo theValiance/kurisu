1545ec7cbd29853d6b67f98e499957972b3604ee
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-05-15 20:27:17 -0400

    Update booru.js

--- booru.js
+++ booru.js
@@ -3,14 +3,17 @@ var request = require('request');
 var xml2js = require('xml2js');
 
 var parser = new xml2js.Parser();
-var url = 'https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=rating:safe+score:>=40';
+
 var timeout = 10000;
-var opts = {
-	url : url,
-	timeout : timeout
-};
+
 
 exports.gelbooru = function(){
+	var url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=rating:safe+score:>=40`;
+	var opts = {
+		url : url,
+		timeout : timeout
+	};
+	
 	return new Promise((resolve, reject) =>{
 		request(opts, function (err, res, body) {
 			if (err) {
@@ -18,7 +21,7 @@ exports.gelbooru = function(){
 				reject(err);
 			}
 			parser.parseString(body, function(err, result){
-				resolve(result['posts']['post'][0]['$']['file_url'].replace('\n', ''));
+				resolve(`https:${result['posts']['post'][0]['$']['file_url'].replace('\n', '')}`);
 			});
 		});
 	});
