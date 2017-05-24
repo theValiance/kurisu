61658ebcbad1a6bfcf6c527b8a301e8a223d16fc
Matthew Becher Nienhaus <spiritofvale@gmail.com>
2017-05-23 20:59:23 -0400

    Update booru.js

--- booru.js
+++ booru.js
@@ -1,14 +1,10 @@
 //script file for interacting with booru apis
 var request = require('request');
-var xml2js = require('xml2js');
-
-var parser = new xml2js.Parser();
 
 var timeout = 10000;
 
-
 exports.gelbooru = function(){
-	var url = `https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=1&tags=rating:safe+score:>=40&pid=${Math.floor((Math.random() * 9995)+1)}`;
+	var url = `https://gelbooru.com/index.php?page=dapi&s=post&json=1&q=index&limit=1&tags=rating:safe+score:>=40&pid=${Math.floor((Math.random() * 9995)+1)}`;
 	var opts = {
 		url : url,
 		timeout : timeout
@@ -20,9 +16,7 @@ exports.gelbooru = function(){
 				console.dir(err);
 				reject(err);
 			}
-			parser.parseString(body, function(err, result){
-				resolve(`https:${result['posts']['post'][0]['$']['file_url'].replace('\n', '')}`);
-			});
+			resolve(`https:${ JSON.parse(res)[0]["file_url"]}`);
 		});
 	});
 };
