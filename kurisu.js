87915a4a78eba46018caa1e971c8487b908e70e7
Valiance <spiritofvale@gmail.com>
2018-01-13 23:27:15 -0500

    Update kurisu.js

--- kurisu.js
+++ kurisu.js
@@ -74,7 +74,7 @@ function playStream(connection, url){
 		const stream = ytdl(url, { filter: 'audioonly' });
 		const dispatcher = connection.playStream(stream);
 		dispatcher.on('end', () => {
-			connection.disconnect();
+			playStream(connection, url);
 		});
 		dispatcher.on('error', (err) => {
 			console.log(err);
